5328cc1caadbe95c59693e714d6ab20493b0fee3
Dan Glastonbury <dan.glastonbury@gmail.com>
2012-01-19 14:00:02 -0800

    Added failing test. Now fix it.

--- test/test_sharm_product.cpp
+++ test/test_sharm_product.cpp
@@ -6,7 +6,7 @@ extern "C" {
 #include "../xhy_sharm.c"
 }
 
-#define SAMPLE_COUNT 1000000
+#define SAMPLE_COUNT 1
 
 namespace {
 	TEST(DXProd3) {
@@ -38,6 +38,43 @@ namespace {
 			{
 				CHECK_EQUAL(t[i], s[i]);
 				/*				if (s[i] != t[i])
+				{
+				printf("sh[%d]: %.17f == %.17f\n", i, s[i], t[i]);
+				difference(s[i], t[i]);
+				}*/
+			}
+		}
+	}
+
+	TEST(DXProd4) {
+		static float s[16];
+		static float t[16];
+
+		static float tmp0[16];
+		static float tmp1[16];
+
+		static float v0[3];
+		static float v1[3];
+
+		for (int n = 0; n < SAMPLE_COUNT; ++n)
+		{
+			random_vec(v0);
+			random_vec(v1);
+
+			xhy_sh4_eval(v0[0], v0[1], v0[2], tmp0);
+			xhy_sh4_eval(v1[0], v1[1], v1[2], tmp1);
+			xhy_sh4_product(tmp0, tmp1, s);
+
+			D3DXVECTOR3 d3dv0(v0);
+			D3DXVECTOR3 d3dv1(v1);
+			D3DXSHEvalDirection(tmp0, 4, &d3dv0);
+			D3DXSHEvalDirection(tmp1, 4, &d3dv1);
+			D3DXSHMultiply4(t, tmp0, tmp1); 
+
+			for (int i = 0; i < 16; ++i)
+			{
+				CHECK_EQUAL(t[i], s[i]);
+				/*if (s[i] != t[i])
 				{
 				printf("sh[%d]: %.17f == %.17f\n", i, s[i], t[i]);
 				difference(s[i], t[i]);
