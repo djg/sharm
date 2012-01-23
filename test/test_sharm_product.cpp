9203e1df66294f8c1975d68076514903f594fcf9
Dan Glastonbury <dan.glastonbury@gmail.com>
2012-01-23 08:58:00 -0800

    Generated working xhy_sh4_product.

--- test/test_sharm_product.cpp
+++ test/test_sharm_product.cpp
@@ -6,80 +6,56 @@ extern "C" {
 #include "../xhy_sharm.c"
 }
 
-#define SAMPLE_COUNT 1
-
 namespace {
 	TEST(DXProd3) {
-		static float s[9];
-		static float t[9];
+		float s[9];
+		float t[9];
 
-		static float tmp0[9];
-		static float tmp1[9];
+		float tmp0[9];
+		float tmp1[9];
 
-		static float v0[3];
-		static float v1[3];
+		float v0[3];
+		float v1[3];
 
-		for (int n = 0; n < SAMPLE_COUNT; ++n)
-		{
-			random_vec(v0);
-			random_vec(v1);
+		random_vec(v0);
+		random_vec(v1);
 
-			xhy_sh3_eval(v0[0], v0[1], v0[2], tmp0);
-			xhy_sh3_eval(v1[0], v1[1], v1[2], tmp1);
-			xhy_sh3_product(tmp0, tmp1, s);
+		xhy_sh3_eval(v0[0], v0[1], v0[2], tmp0);
+		xhy_sh3_eval(v1[0], v1[1], v1[2], tmp1);
+		xhy_sh3_product(tmp0, tmp1, s);
 
-			D3DXVECTOR3 d3dv0(v0);
-			D3DXVECTOR3 d3dv1(v1);
-			D3DXSHEvalDirection(tmp0, 3, &d3dv0);
-			D3DXSHEvalDirection(tmp1, 3, &d3dv1);
-			D3DXSHMultiply3(t, tmp0, tmp1); 
+		D3DXVECTOR3 d3dv0(v0);
+		D3DXVECTOR3 d3dv1(v1);
+		D3DXSHEvalDirection(tmp0, 3, &d3dv0);
+		D3DXSHEvalDirection(tmp1, 3, &d3dv1);
+		D3DXSHMultiply3(t, tmp0, tmp1); 
 
-			for (int i = 0; i < 9; ++i)
-			{
-				CHECK_EQUAL(t[i], s[i]);
-				/*				if (s[i] != t[i])
-				{
-				printf("sh[%d]: %.17f == %.17f\n", i, s[i], t[i]);
-				difference(s[i], t[i]);
-				}*/
-			}
-		}
+		CHECK_ARRAY_EQUAL(t, s, 9);
 	}
 
 	TEST(DXProd4) {
-		static float s[16];
-		static float t[16];
+		float s[16];
+		float t[16];
 
-		static float tmp0[16];
-		static float tmp1[16];
+		float tmp0[16];
+		float tmp1[16];
 
-		static float v0[3];
-		static float v1[3];
+		float v0[3];
+		float v1[3];
 
-		for (int n = 0; n < SAMPLE_COUNT; ++n)
-		{
-			random_vec(v0);
-			random_vec(v1);
+		random_vec(v0);
+		random_vec(v1);
 
-			xhy_sh4_eval(v0[0], v0[1], v0[2], tmp0);
-			xhy_sh4_eval(v1[0], v1[1], v1[2], tmp1);
-			xhy_sh4_product(tmp0, tmp1, s);
+		xhy_sh4_eval(v0[0], v0[1], v0[2], tmp0);
+		xhy_sh4_eval(v1[0], v1[1], v1[2], tmp1);
+		xhy_sh4_product(tmp0, tmp1, s);
 
-			D3DXVECTOR3 d3dv0(v0);
-			D3DXVECTOR3 d3dv1(v1);
-			D3DXSHEvalDirection(tmp0, 4, &d3dv0);
-			D3DXSHEvalDirection(tmp1, 4, &d3dv1);
-			D3DXSHMultiply4(t, tmp0, tmp1); 
+		D3DXVECTOR3 d3dv0(v0);
+		D3DXVECTOR3 d3dv1(v1);
+		D3DXSHEvalDirection(tmp0, 4, &d3dv0);
+		D3DXSHEvalDirection(tmp1, 4, &d3dv1);
+		D3DXSHMultiply4(t, tmp0, tmp1); 
 
-			for (int i = 0; i < 16; ++i)
-			{
-				CHECK_EQUAL(t[i], s[i]);
-				/*if (s[i] != t[i])
-				{
-				printf("sh[%d]: %.17f == %.17f\n", i, s[i], t[i]);
-				difference(s[i], t[i]);
-				}*/
-			}
-		}
+		CHECK_ARRAY_EQUAL(t, s, 16);
 	}
 }
